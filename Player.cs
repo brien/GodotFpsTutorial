d8f9569ee1764bdcab7bd134682937fc72672272
Brien Smith Martinez <bsmartinez@gmail.com>
2021-07-07 18:25:45 +0200

    Add PhysicsProcess and Input override methods to Player, and supporting private functions to support input and movement processing.

--- Player.cs
+++ Player.cs
@@ -36,9 +36,101 @@ public class Player : KinematicBody
         Input.SetMouseMode(Input.MouseMode.Captured);
     }
 
-//  // Called every frame. 'delta' is the elapsed time since the previous frame.
-//  public override void _Process(float delta)
-//  {
-//      
-//  }
+    public override void _PhysicsProcess(float delta)
+    {
+        ProcessInput(delta);
+        ProcessMovement(delta);
+    }
+
+    private void ProcessInput(float delta)
+    {
+        //  -------------------------------------------------------------------
+        //  Walking
+        _dir = new Vector3();
+        Transform camXform = _camera.GlobalTransform;
+
+        Vector2 inputMovementVector = new Vector2();
+
+        if (Input.IsActionPressed("movement_forward"))
+            inputMovementVector.y += 1;
+        if (Input.IsActionPressed("movement_backward"))
+            inputMovementVector.y -= 1;
+        if (Input.IsActionPressed("movement_left"))
+            inputMovementVector.x -= 1;
+        if (Input.IsActionPressed("movement_right"))
+            inputMovementVector.x += 1;
+
+        inputMovementVector = inputMovementVector.Normalized();
+
+        // Basis vectors are already normalized.
+        _dir += -camXform.basis.z * inputMovementVector.y;
+        _dir += camXform.basis.x * inputMovementVector.x;
+        //  -------------------------------------------------------------------
+
+        //  -------------------------------------------------------------------
+        //  Jumping
+        if (IsOnFloor())
+        {
+            if (Input.IsActionJustPressed("movement_jump"))
+                _vel.y = JumpSpeed;
+        }
+        //  -------------------------------------------------------------------
+
+        //  -------------------------------------------------------------------
+        //  Capturing/Freeing the cursor
+        if (Input.IsActionJustPressed("ui_cancel"))
+        {
+            if (Input.GetMouseMode() == Input.MouseMode.Visible)
+                Input.SetMouseMode(Input.MouseMode.Captured);
+            else
+                Input.SetMouseMode(Input.MouseMode.Visible);
+        }
+        //  -------------------------------------------------------------------
+    }
+
+    private void ProcessMovement(float delta)
+    {
+        _dir.y = 0;
+        _dir = _dir.Normalized();
+
+        _vel.y += delta * Gravity;
+
+        Vector3 hvel = _vel;
+        hvel.y = 0;
+
+        Vector3 target = _dir;
+
+        target *= MaxSpeed;
+
+        float accel;
+        if (_dir.Dot(hvel) > 0)
+            accel = Accel;
+        else
+            accel = Deaccel;
+
+        hvel = hvel.LinearInterpolate(target, accel * delta);
+        _vel.x = hvel.x;
+        _vel.z = hvel.z;
+        _vel = MoveAndSlide(_vel, new Vector3(0, 1, 0), false, 4, Mathf.Deg2Rad(MaxSlopeAngle));
+    }
+
+    public override void _Input(InputEvent @event)
+    {
+        if (@event is InputEventMouseMotion && Input.GetMouseMode() == Input.MouseMode.Captured)
+        {
+            InputEventMouseMotion mouseEvent = @event as InputEventMouseMotion;
+            _rotationHelper.RotateX(Mathf.Deg2Rad(mouseEvent.Relative.y * MouseSensitivity));
+            RotateY(Mathf.Deg2Rad(-mouseEvent.Relative.x * MouseSensitivity));
+
+            Vector3 cameraRot = _rotationHelper.RotationDegrees;
+            cameraRot.x = Mathf.Clamp(cameraRot.x, -70, 70);
+            _rotationHelper.RotationDegrees = cameraRot;
+        }
+    }
+
+    //  // Called every frame. 'delta' is the elapsed time since the previous frame.
+    //  public override void _Process(float delta)
+    //  {
+    //      
+    //  }
 }
