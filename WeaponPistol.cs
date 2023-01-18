bc7bf1a4b18b6029931911cbadc4c480ca0a88ba
Brien Smith Martinez <bsmartinez@gmail.com>
2023-01-18 21:00:26 +0100

    (does not work) Add WeaponPistol script

---
+++ WeaponPistol.cs
@@ -0,0 +1,40 @@
+using Godot;
+using System;
+
+public class WeaponPistol : Spatial
+{
+    // Declare member variables here.
+    private const int DAMAGE = 15;
+
+    private const string IDLE_ANIM_NAME = "Pistol_idle";
+    private const string FIRE_ANIM_NAME = "Pistol_fire";
+
+    private bool is_weapon_enabled = false;
+
+    private PackedScene bullet_scene = (PackedScene)ResourceLoader.Load("Bullet_Scene.tscn");
+
+    private Node player_node = null;
+
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready()
+    {
+        
+    }
+
+    public void FireWeapon()
+    {
+        var clone = bullet_scene.Instance();
+        var scene_root = GetTree().Root.GetChildren()[0];
+        AddChild(clone);
+
+        clone. = self.global_transform;
+        clone.scale = Vector3(4, 4, 4);
+        clone.BULLET_DAMAGE = DAMAGE;
+    }
+
+//  // Called every frame. 'delta' is the elapsed time since the previous frame.
+//  public override void _Process(float delta)
+//  {
+//      
+//  }
+}
